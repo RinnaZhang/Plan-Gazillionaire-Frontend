bc004cbe3432320e87afe299d94f420bd0936751
Allan Zhan <allanzhan@10-17-73-231.dynapool.wireless.nyu.edu>
2024-11-04 14:57:19 -0500

    solved polymarket api pagination issue

--- polymarketapi.py
+++ polymarketapi.py
@@ -3,38 +3,57 @@ import ast
 #gets the information from polybets api
 #formats information into a list for database entry
 
-r = requests.get("https://gamma-api.polymarket.com/events?closed=false")
-response = r.json()
+base_url = "https://gamma-api.polymarket.com/events"
+params = {
+    "closed": "false",
+    "limit": 100,
+    "offset": 0
+}
 
 political_events = {}
-for event in response:
-    print(event['title']) #just to see how many events are being shown
-    list_tags = [tag['slug'] for tag in event['tags']]
-    if any("politics" in tag for tag in list_tags): #determines if the event is tagged politics
-        info_list = []
-        markets = {}
-        print(event['title']) #the ones with name printed twice are political events
+count = 0
+
+while True:
+    r = requests.get(base_url, params=params)
+    response = r.json()
+
+    if not response:
+        break
+
+    for event in response:
+        count += 1 #increase count to see how many responses
+        list_tags = [tag['slug'] for tag in event['tags']]
+    
+        if any("politics" in tag for tag in list_tags): #determines if the event is tagged politics
+            info_list = []
+            markets = {}
         
         #add info
-        political_events[event['id']] = info_list
-        info_list.append(event['title'])
-        info_list.append(event['endDate']) 
-        info_list.append('polymarket') 
-        info_list.append('open') 
-        for market in event['markets']:
-            markets['question'] = market['question']
-            print(market['outcomes']) #to ensure the outcomes are correct
-            clean_outcomes = ast.literal_eval(market['outcomes']) 
-            clean_outcomePrices = ast.literal_eval(market['outcomePrices'])
-            i = 0
-            while i < len(clean_outcomes):
-               key = f'outcome {i+1}' 
-               key2 = f'outcome {i+1} price'
-               markets[key] = clean_outcomes[i]
-               markets[key2] = clean_outcomePrices[i]
-               i = i + 1
-            print(markets) #checks markets to ensure data is formatted correctly
-            info_list.append(markets)
+            political_events[event['id']] = info_list
+            info_list.append(event['title'])
+            info_list.append(event['endDate']) 
+            info_list.append('polymarket') 
+            info_list.append('open') 
+        
+            for market in event['markets']:
+                markets['question'] = market['question']
+                
+                clean_outcomes = ast.literal_eval(market['outcomes'])
+                if 'outcomePrices' in market:
+                    clean_outcomePrices = ast.literal_eval(market['outcomePrices'])
+            
+                i = 0
+                while i < len(clean_outcomes):
+                    key = f'outcome {i+1}' 
+                    key2 = f'outcome {i+1} price'
+                    markets[key] = clean_outcomes[i]
+                    markets[key2] = clean_outcomePrices[i]
+                    i = i + 1
+
+                info_list.append(markets)
+    params["offset"] += 100
 
 print(political_events) #check to make sure dict is correct
+print(count) #check number of results
+
        
