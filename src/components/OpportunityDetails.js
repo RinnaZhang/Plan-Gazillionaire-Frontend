d62803be56e804d204c80bc07f61686e78385010
Rinna <rinnazhang79@gmail.com>
2024-11-11 22:23:35 -0500

    Update OpportunityDetails.js

--- src/components/OpportunityDetails.js
+++ src/components/OpportunityDetails.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import '../index.css';
 
-
 function OpportunityDetails({ opportunity }) {
-
+    if (!opportunity) {
+    return <p>No details available</p>;
+  }
+  
   return (
     <div className="box-color box-neon-green-light p-4 rounded-lg shadow-md transition duration-300 ease-in-out">
       <h3 className="text-2xl header-color font-semibold mb-2">{opportunity.title}</h3>
       <p className="text-dark-blue mb-1"><strong>Bet 1:</strong> {opportunity.bet1}</p>
       <p className="text-dark-blue mb-1"><strong>Bet 2:</strong> {opportunity.bet2}</p>
       <p className="text-dark-blue"><strong>Total Profit:</strong> {opportunity.profit}</p>
+      <p className="text-dark-blue"><strong>Timestamp:</strong> {opportunity.timestamp}</p>
     </div>
   );
-  
 }
 
 export default OpportunityDetails;
